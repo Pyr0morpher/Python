aafe834ac0f376730e004d2b77f825a072a76cae
Pyro <32312484+Pyr0morpher@users.noreply.github.com>
2020-02-04 10:55:47 -0600

    Create SumMethodGoodman.py

---
+++ SumMethodGoodman.py
@@ -0,0 +1,8 @@
+def print_sum(a,b,c):
+  print("The sum: " + str(a + b + c))
+
+first = int(input("Enter the 1st number: "))
+second = int(input("Enter the 2nd number: "))
+third = int(input("Enter the 3rd number: "))
+
+print_sum(first,second,third)
